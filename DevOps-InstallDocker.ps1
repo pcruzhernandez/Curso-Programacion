1dd6a28a0d0d41ec84e35ef6a1342f9f9511d1d1
Michael Megel <Michael.Megel@CosmoConsult.com>
2018-04-18 15:30:50 +0200

    Workshop preparation (Git & Infrastructure)

---
+++ DevOps-InstallDocker.ps1
@@ -0,0 +1,9 @@
+﻿Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
+Install-Package -Name docker -ProviderName DockerMsftProvider
+Restart-Computer -Force
+
+##### Nav-ContainerHelper
+
+Install-Module navcontainerhelper -force
+
+New-CSideDevContainer -accept_eula -containerName "nav-dev" -auth NavUserPassword -imageName "microsoft/bcsandbox:de" -doNotExportObjectsToText
