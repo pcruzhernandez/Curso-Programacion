ea7ceb9c720e9af8acbfbc272d4ac1fa101d6c9f
Michael Megel <Michael.Megel@CosmoConsult.com>
2018-05-31 08:53:14 +0200

    DevOps Docker

---
+++ 04-DevOps-Docker/Solution/01-SetupContainer/CreateContainer.ps1
@@ -0,0 +1,14 @@
+﻿$authUser    = 'student'
+$authPass    = 'WS-VSCode2018'
+$secpasswd   = ConvertTo-SecureString $authPass -AsPlainText -Force
+$credential  = New-Object System.Management.Automation.PSCredential ($authUser, $secpasswd)
+
+New-NavContainer -accept_eula `
+                 -accept_outdated `
+                 -containerName "nav-dev" `
+                 -imageName "microsoft/dynamics-nav:2018-cu5-de" `
+                 -licenseFile "<OutSide-Folder>\License.flf" `
+                 -credential $credential `
+                 -includeCSide `
+                 -enableSymbolLoading `
+                 -auth NavUserPassword                 
